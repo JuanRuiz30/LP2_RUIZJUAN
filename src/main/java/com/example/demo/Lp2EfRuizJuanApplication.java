d0405158f5e75d87a921e00741bbc4f7c6e4882c
Juan Ruiz <djuanuiz@gmail.com>
2024-07-03 20:36:32 -0500

    iniciando examen final juan ruiz

---
+++ src/main/java/com/example/demo/Lp2EfRuizJuanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2EfRuizJuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2EfRuizJuanApplication.class, args);
+	}
+
+}
